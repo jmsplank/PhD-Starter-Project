dc54579b81876b10b66747d972d9540e3ea6588e
James Plank <plankjames@gmail.com>
2021-01-25 19:07:19 +0000

    Fitting lines to spectrum

--- src/magSpec/auto_grads.py
+++ src/magSpec/auto_grads.py
@@ -1,7 +1,10 @@
 import pyspedas
 import numpy as np
 from pytplot import tplot, data_quants
-from ..distribution_function.rotate import rot_to_b
+from phdhelper.math import transforms
+import fsm_magSpec
+from scipy.optimize import curve_fit
+import matplotlib.pyplot as plt
 
 trange = ["2016-12-09/09:01:36", "2016-12-09/09:07:00"]  # Interval 1
 # trange = ["2016-12-09/09:26:24", "2016-12-09/09:34:58"]  # Interval 2
@@ -13,34 +16,98 @@ pyspedas.mms.fpi(trange, probe, data_rate)
 pyspedas.mms.fgm(trange, probe, data_rate)
 
 # tplot(["mms1_fgm_b_gse_brst_l2", "mms1_dis_tempperp_brst"])
+def lengths(s="i"):
+    if s == "i":
+        print("---->----IONS----<----")
+    else:
+        print("---->----ELEC----<----")
 
-print("---->----IONS----<----")
-print("Getting number diensity mean")
-n_i = data_quants["mms1_dis_numberdensity_brst"].values.mean()
-print("Calc ion plasma freq")
-i_const = 1.32e3
-mu = 1
-Z = 1
-# https://en.wikipedia.org/wiki/Plasma_parameters#Fundamental_plasma_parameters
-omega_pi = i_const * Z * np.sqrt(n_i / mu)
-print("Calc ion inert. len")
-p_i = 2.99792458e8 / omega_pi
-print(p_i)
+    n = data_quants[f"mms1_d{s}s_numberdensity_brst"].values.mean()
+    const = 1.32e3 if s == "i" else 5.64e4
+    # https://en.wikipedia.org/wiki/Plasma_parameters#Fundamental_plasma_parameters
+    omega_p = const * np.sqrt(n)
+    p = 2.99792458e8 / omega_p
+    p /= 1e3
+    print(f"Inertial length: {p:.3f}km")
 
-print("Calc ion gyrorad.")
-print("Calc ion gyrofreq.")
-v_i = data_quants["mms1_dis_bulkv_gse_brst"].values.mean(axis=0)
-B = data_quants["mms1_fgm_b_gse_brst_l2"].values
-BT = B[:, 3].mean()
-B = B[:, :3].mean(axis=0)
-v_i = rot_to_b(B, v_i)
-v_i = np.linalg.norm(v_i[:2])
-print(v_i)
-# B = data_quants["mms1_fgm_b_gse_brst_l2"].values[:, 3].mean() * 1e-9
-# print(f"{B/1e-9:0.3f}nT")
-# T_i = data_quants["mms1_dis_tempperp_brst"].values.mean() * (
-#     1.60217662e-19 / 1.38064852e-23
-# )
-# print(f"{T_i:0.3f}K")
-# rho_i = 1.02e2 * ((mu * T_i) ** 0.5) / (Z * B)
-# print(rho_i)
+    v = data_quants[f"mms1_d{s}s_bulkv_gse_brst"].values.mean(axis=0)
+    B = data_quants["mms1_fgm_b_gse_brst_l2"].values * 1e-9
+    # BT = B[:, 3].mean()
+    B = B[:, :3].mean(axis=0)
+    v = transforms.rot_to_b(B, v)
+    v = np.linalg.norm(v[:2])
+    omega_c = 1.60217662e-19 * B[2] / (1.6726219e-27 if s == "i" else 9.10938356e-31)
+    rho = v / omega_c
+    print(f"Gyroradius: {rho:.3f}km")
+
+    limit = p if p > rho else rho
+    print("---->----<<>>----<----")
+    return limit
+
+
+k, y = fsm_magSpec.load_data(trange, probe)
+i_limit = np.argmin(abs((1.0 / lengths("i")) - k))
+e_limit = np.argmin(abs((1.0 / lengths("e")) - k))
+print(i_limit, e_limit)
+
+log_k = np.log10(k)
+log_y = np.log10(y)
+
+
+def line(x, m, c):
+    return c + m * x
+
+
+def grad(lower=None, upper=None):
+    sl = slice(lower, upper)
+    grad, pcov = curve_fit(line, log_k[sl], log_y[sl])
+    err_grad = np.sqrt(np.diag(pcov))
+    return grad, err_grad, sl
+
+
+g0 = grad(None, i_limit)
+g1 = grad(i_limit, e_limit)
+g2 = grad(e_limit, None)
+
+print(g0, g1, g2, sep="\n")
+
+ax1, ax2 = fsm_magSpec.plot(
+    k, y, vlines=np.array([1.0 / lengths("i"), 1.0 / lengths("e")]).flatten()
+)
+
+
+def plotfitted(var, color):
+    mainLine = var[0]
+    minLine = var[0] - np.array([0, var[1][1]])
+    maxLine = var[0] + np.array([0, var[1][1]])
+
+    def gen_xy(grad):
+        x = log_k[var[2]][[0, -1]]
+        y = line(x, grad[0], grad[1])
+        x = 10 ** x
+        y = 10 ** y
+        return x, y
+
+    mainLine = gen_xy(mainLine)
+    minLine = gen_xy(minLine)
+    maxLine = gen_xy(maxLine)
+
+    ax1.plot(mainLine[0], mainLine[1], color=color)
+    ax2.plot(mainLine[0], mainLine[1] * mainLine[0] ** 2.7, color=color)
+
+    ax1.fill_between(mainLine[0], minLine[1], maxLine[1], color=color, alpha=0.2)
+    ax2.fill_between(
+        mainLine[0],
+        minLine[1] * mainLine[0] ** 2.7,
+        maxLine[1] * mainLine[0] ** 2.7,
+        color=color,
+        alpha=0.2,
+    )
+
+
+plotfitted(g0, "orange")
+plotfitted(g1, "red")
+plotfitted(g2, "skyblue")
+
+
+plt.show()
--- src/magSpec/fsm_magSpec.py
+++ src/magSpec/fsm_magSpec.py
@@ -6,48 +6,65 @@ import numpy as np
 import pyspedas
 from pytplot import data_quants
 
-trange = ["2016-12-09/09:01:36", "2016-12-09/09:07:00"]  # Interval 1
-# trange = ["2016-12-09/09:26:24", "2016-12-09/09:34:58"]  # Interval 2
-probe = "1"
-# probe = "1"
-data_rate = "brst"
 
-mms_fsm = pyspedas.mms.fsm(trange=trange, probe=probe, time_clip=True, level="l3")
+def load_data(trange, probe):
+    pyspedas.mms.fsm(trange=trange, probe=probe, time_clip=True, level="l3")
 
-fsm_B = data_quants["mms1_fsm_b_gse_brst_l3"].values
-time_dist = data_quants["mms1_fsm_b_gse_brst_l3"].coords["time"].values
-td = time_dist[1] - time_dist[0]
+    fsm_B = data_quants["mms1_fsm_b_gse_brst_l3"].values
+    time_dist = data_quants["mms1_fsm_b_gse_brst_l3"].coords["time"].values
+    td = time_dist[1] - time_dist[0]
 
-# Correct for missing data
-Y = {}
-for i in range(3):
-    B = fsm_B[:, i] * 10e-9
-    finiteMask = np.isfinite(B)
-    print((1 - finiteMask).sum())
-    B = np.interp(time_dist, time_dist[finiteMask], B[finiteMask])
-    B -= B.mean()
-    Hann = np.hanning(len(B)) * B
-    Yi = np.fft.fft(Hann)
-    freq = np.fft.fftfreq(len(B), time_dist[1] - time_dist[0])
-    meanv = np.load("src/magSpec/meanv.npy", allow_pickle=True)
-    k = 2 * np.pi * freq / meanv
-    k = k[freq > 0]
-    Y[["x", "y", "z"][i]] = (np.power(abs(Yi), 2) * 10e9 / (1.0 / td))[freq > 0]
+    # Correct for missing data
+    Y = {}
+    for i in range(3):
+        B = fsm_B[:, i] * 10e-9
+        finiteMask = np.isfinite(B)
+        print((1 - finiteMask).sum())
+        B = np.interp(time_dist, time_dist[finiteMask], B[finiteMask])
+        B -= B.mean()
+        Hann = np.hanning(len(B)) * B
+        Yi = np.fft.fft(Hann)
+        freq = np.fft.fftfreq(len(B), time_dist[1] - time_dist[0])
+        meanv = np.load("src/magSpec/meanv.npy", allow_pickle=True)
+        k = 2 * np.pi * freq / meanv
+        k = k[freq > 0]
+        Y[["x", "y", "z"][i]] = (np.power(abs(Yi), 2) * 10e9 / (1.0 / td))[freq > 0]
 
-y = np.sum([Y[i] for i in ["x", "y", "z"]], axis=0)
+    y = np.sum([Y[i] for i in ["x", "y", "z"]], axis=0)
+    return k, y
 
-plt.subplot(2, 1, 1)
-plt.loglog(k, y)
-plt.xlabel("f [Hz]")
-plt.ylabel("Magnetic spectrum [nT²Hz⁻¹]")
 
-plt.subplot(2, 1, 2)
-plt.loglog(k, y * k ** (2.7))
-plt.xlabel("k[km$^{-1}$]")
-plt.ylabel(r"Magnetic Spectrum $\times k^{2.7}$")
+def plot(k, y, vlines=None, slope=2.7):
+    ax1 = plt.subplot(2, 1, 1)
+    ax1.loglog(k, y)
+    ax1.set_xlabel("f [Hz]")
+    ax1.set_ylabel("Magnetic spectrum [nT²Hz⁻¹]")
+    if vlines is not None:
+        ymin = min(y)
+        ymax = max(y)
+        ax1.vlines(vlines, ymin, ymax)
 
-gen_fmt = dt.strftime(dt.now(), "%H%M%S_%a%d%b")
-plt.subplot(2, 1, 1)
-plt.title(f"Generated: {gen_fmt}")
-plt.savefig(f"src/magSpec/fsm_magSpec_{gen_fmt}.png")
-plt.show()
+    ax2 = plt.subplot(2, 1, 2)
+    yk = y * k ** slope
+    ax2.loglog(k, yk)
+    ax2.set_xlabel("k[km$^{-1}$]")
+    ax2.set_ylabel(r"Magnetic Spectrum $\times k^{2.7}$")
+    if vlines is not None:
+        ymin = min(yk)
+        ymax = max(yk)
+        ax2.vlines(vlines, ymin, ymax)
+    return ax1, ax2
+
+
+if __name__ == "__main__":
+    trange = ["2016-12-09/09:01:36", "2016-12-09/09:07:00"]  # Interval 1
+    # trange = ["2016-12-09/09:26:24", "2016-12-09/09:34:58"]  # Interval 2
+    probe = "1"
+    # probe = "1"
+    data_rate = "brst"
+    k, y = load_data(trange, probe)
+    plot(k, y)
+
+    plt.title(f"Generated: {gen_fmt}")
+    plt.savefig(f"src/magSpec/fsm_magSpec_{gen_fmt}.png")
+    plt.show()
